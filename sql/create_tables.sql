be45e1a6110a0b03b21189b75cea616045f6f225
Jake Harding <jharding@twitter.com>
2014-06-11 02:13:48 -0700

    Add users table.

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,4 +1,11 @@
-CREATE TABLE Notifications (
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  email varchar(254) NOT NULL,
+  password char(60) NOT NULL,
+  UNIQUE(email)
+);
+
+CREATE TABLE notifications (
   id SERIAL PRIMARY KEY, 
   summary varchar(128) NOT NULL
 );
