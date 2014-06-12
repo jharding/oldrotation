6e8edc9989849f3e793fb10943d462bb397ec0dd
Jake Harding <jharding@twitter.com>
2014-06-12 01:13:07 -0700

    Clean up templates.

--- utils/render.js
+++ utils/render.js
@@ -1,16 +1,21 @@
-var FILE_EXT, jade, jadeRender, render, thunkify;
+var _, FILE_EXT, jade, jadeRender, path, OPTS_MIXIN, render, thunkify;
 
 // external modules
+_ = require('underscore');
 jade = require('jade');
+path = require('path');
 thunkify = require('thunkify');
 
 // constants
 FILE_EXT = '.jade';
+OPTS_MIXIN = {
+  basedir: path.join(__dirname, '../templates')
+};
 
 jadeRender = thunkify(jade.renderFile);
 
 // export
 render = module.exports = function* render(path, ctx) {
   path += FILE_EXT;
-  return yield jadeRender(path, ctx);
+  return yield jadeRender(path, _.extend(ctx, OPTS_MIXIN));
 };
--- views/users.js
+++ views/users.js
@@ -6,14 +6,14 @@ render = appRequire('utils/render');
 // export
 views = module.exports = {
   loginForm: function* loginForm() {
-    return yield render('templates/login', {
+    return yield render('templates/user/login', {
       csrfParam: this.csrfParam,
       csrfToken: this.csrf,
     });
   },
 
   signupForm: function* signupForm() {
-    return yield render('templates/signup', {
+    return yield render('templates/user/signup', {
       csrfParam: this.csrfParam,
       csrfToken: this.csrf
     });
