43295dd2fea20c74414d81950f8ddd3eba7f6391
Jake Harding <jharding@twitter.com>
2014-06-12 00:45:30 -0700

    Clean up auth util.

--- utils/auth.js
+++ utils/auth.js
@@ -1,34 +1,38 @@
-var _ = require('underscore'),
-    co = require('co'),
-    compose = require('koa-compose'),
-    LocalStrategy = require('passport-local').Strategy,
-    passport = require('koa-passport');
+var _, auth, co, compose, LocalStrategy, passport, userRepo;
 
-var users = require('../models/users.js');
+// external modules
+_ = require('underscore');
+co = require('co');
+compose = require('koa-compose');
+LocalStrategy = require('passport-local').Strategy;
+passport = require('koa-passport');
+
+// internal modules
+userRepo = appRequire('models/users');
 
 passport.serializeUser(co(serializeUser));
 passport.deserializeUser(co(deserializeUser));
 passport.use(new LocalStrategy({ usernameField: 'email' }, co(verify)));
 
-module.exports = {
-  initialize: initialize,
-  auth: passport.authenticate.bind(passport)
+// export
+auth = module.exports = {
+  initialize: function initialize() {
+    return compose([
+      passport.initialize(),
+      passport.session(),
+      addHelpersToCtx()
+    ]);
+  },
+
+  authenticate: passport.authenticate.bind(passport)
 };
 
-function initialize() {
-  return compose([
-    passport.initialize(),
-    passport.session(),
-    addUserToCtx()
-  ]);
-}
-
-function addUserToCtx(o) {
+function addHelpersToCtx(o) {
   var name = (o && o.name) || 'user';
 
-  return addUserToCtx;
+  return addHelpersToCtx;
 
-  function* addUserToCtx(next) {
+  function* addHelpersToCtx(next) {
     this[name] = this.req.user;
     yield next;
   }
@@ -39,12 +43,12 @@ function* serializeUser(user) {
 }
 
 function* deserializeUser(id) {
-  return yield users.findById(id);
+  return yield userRepo.findById(id);
 }
 
 function* verify(email, password) {
     var validCreds;
 
-    validCreds = yield users.verifyCredentials(email, password);
-    return validCreds ? yield users.findByEmail(email) : null;
+    validCreds = yield userRepo.verifyCredentials(email, password);
+    return validCreds ? yield userRepo.findByEmail(email) : null;
 }
