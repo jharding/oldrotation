7727f1d9b1ab5591de188f0aad695f9bbebcce00
Jake Harding <jharding@twitter.com>
2014-06-12 00:46:39 -0700

    Add collection of middleware for endpoints.

---
+++ utils/endpoint_mw.js
@@ -0,0 +1,33 @@
+// export
+module.exports = {
+  logout: function* logout(next) {
+    this.logout();
+    yield next;
+  },
+
+  redirectTo: function redirectTo() {
+    var args = [].slice.call(arguments, 0);
+
+    return function* redirectTo() {
+      this.redirect.apply(this, args);
+    };
+  },
+
+  requireAuth: function* requireAuth(next) {
+    if (this.isUnauthenticated()) {
+      this.redirect('/login');
+      return;
+    }
+
+    yield next;
+  },
+
+  requireUnauth: function* requireUnauth(next) {
+    if (this.isAuthenticated()) {
+      this.redirect('back');
+      return;
+    }
+
+    yield next;
+  }
+};
